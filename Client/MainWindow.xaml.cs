af8638209ef1a804f7e8e47b25a34f390bc97459
Rəsul <71267982+ResuIl@users.noreply.github.com>
2023-03-18 19:09:29 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace Client;
+
+public partial class MainWindow : Window
+{
+    UdpClient client;
+    IPEndPoint remoteEP;
+    bool isPlaying = false;
+    public MainWindow()
+    {
+        InitializeComponent();
+        client = new UdpClient();
+        remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            var buffer = new byte[ushort.MaxValue - 29];
+            await client.SendAsync(buffer, buffer.Length, remoteEP);
+            var list = new List<byte>();
+            var maxlen = buffer.Length;
+            var len = 0;
+            while (true)
+            {
+                do
+                {
+                    try
+                    {
+                        var result = await client.ReceiveAsync();
+                        buffer = result.Buffer;
+                        len = buffer.Length;
+                        list.AddRange(buffer);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                } while (len == maxlen);
+                var image = LoadImage(list.ToArray());
+                if (image != null)
+                    Player.Source = image;
+
+                list.Clear();
+            }
+        } 
+    }
+    private static BitmapImage? LoadImage(byte[] imageData)
+    {
+        var image = new BitmapImage();
+        image.BeginInit();
+        image.StreamSource = new MemoryStream(imageData);
+        image.CacheOption = BitmapCacheOption.OnLoad;
+        image.EndInit();
+        return image;
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+UdpClient server = new UdpClient(45678);
+var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+while (true)
+{
+    var result = await server.ReceiveAsync();
+    new Task(async () =>
+    {
+        remoteEP = result.RemoteEndPoint;
+        while (true)
+        {
+            var imgByte = ImageToByte(TakeScreenShot());
+            var chunks = imgByte.Chunk(ushort.MaxValue - 29);
+            foreach (var chunk in chunks)
+                await server.SendAsync(chunk, chunk.Length, remoteEP);
+        }
+
+    }).Start();
+}
+
+byte[] ImageToByte(Image img)
+{
+    using (MemoryStream ms = new MemoryStream())
+    {
+        img.Save(ms, ImageFormat.Jpeg);
+        return ms.ToArray();
+    }
+}
+
+Image TakeScreenShot()
+{
+    Rectangle bounds = Screen.PrimaryScreen.Bounds;
+    Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+    using (Graphics graphics = Graphics.FromImage(bitmap))
+    {
+        graphics.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size);
+    }
+    return bitmap;
+}
